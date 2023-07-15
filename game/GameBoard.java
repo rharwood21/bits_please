2576449e537117cad782c96e7b02032a0f8f1681
Riley J Harwood <rharwood17@gmail.com>
2023-07-15 16:14:55 -0500

    Getting smarter about the board. Still just getting started.

--- game/GameBoard.java
+++ game/GameBoard.java
@@ -1,8 +1,10 @@
 package game;
 
+import java.awt.*;
+
 public class GameBoard {
     // a 2D array to create the coordinate system for the board
-    private static int[][] coordinateSystem = new int[9][9];
+    private static Square[][] board;
     // creating logical values for valid squares on the gameboard
 
     public GameBoard() {
@@ -10,13 +12,28 @@ public class GameBoard {
         *
         *
         * */
-        // Making the corners of the board 6 - "Roll Again"
-        coordinateSystem[0][0] = 6;
-        coordinateSystem[8][0] = 6;
-        coordinateSystem[8][8] = 6;
-        coordinateSystem[0][8] = 6;
-        // Making the center square 7 - "Trivial Compute"
-        coordinateSystem[4][4] = 7;
+        String color = "";
+        String type = "";
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                switch (i % 4) {
+                    case 0:
+                        color = "R";
+                    case 1:
+                        color = "Y";
+                    case 2:
+                        color = "G";
+                    case 3:
+                        color = "B";
+                }
+                if ((i == 0 || i == 9) && (j == 0 || j == 9)) {
+                    type = "Roll";
+                    //TODO: make enums for types and colors
+                }
+                board[i][j] = new Square(new Point(i, j), color, type);
+            }
+        }
+
         // Assigning the squares a value between 1-4 to make the colors later
         // I feel really dumb, but I can't think of a loop to describe the order of colors...
         // ...might have to hardcode them
---
+++ game/Square.java
@@ -0,0 +1,16 @@
+package game;
+
+import java.awt.*;
+
+public class Square {
+    private Point boardPosition;
+    private String color;
+    private String type;
+
+    public Square(Point boardPosition, String color, String type) {
+        this.boardPosition = boardPosition;
+        this.color = color;
+        this.type = type;
+    }
+}
+
