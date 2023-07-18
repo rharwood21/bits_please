a91658e603e5326be0805f4c70da817dcf225a72
karisconnect <elizabethp.karis@gmail.com>
2023-07-18 13:01:48 -0400

    consistent size/font for section titles

--- src/main/java/pages/InstructionsPage.java
+++ src/main/java/pages/InstructionsPage.java
@@ -35,23 +35,23 @@ public class InstructionsPage extends JFrame {
         setLayout(new BorderLayout());
 
         JLabel objectTitleLabel = new JLabel("Object of the Game", SwingConstants.CENTER);
-        objectTitleLabel.setFont(new Font("Roboto", Font.BOLD, 30));
+        objectTitleLabel.setFont(new Font("Roboto", Font.BOLD, 20));
         // TODO undo hardcode
         JTextArea objectText = setTextArea("To move along the circular track and the spokes correctly answering " +
                 "questions, and to collect colored wedges for correctly answering questions in each of the four category" +
                 " headquarters (at the base of each spoke). To win, a player returns to the square hub and correctly " +
                 "answers the game-winning question in a category chosen by the other players.");
         JLabel setupLabel = new JLabel("Game Setup", SwingConstants.CENTER);
-        setupLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        setupLabel.setFont(new Font("Roboto", Font.BOLD, 20));
         JTextArea setupText = setTextArea("Select four unique categories and corresponding colors. You can select " +
                 "either the default questions or use categories with questions created by teachers. Roll the die; the " +
                 "high roller goes first (the system does this automatically. Al players start in the square hub at the " +
                 "center of the board and move down one of the spokes and out onto the square track. Play moves in either" +
                 " direction around the track, clockwise or counterclockwise. ");
         JLabel gamePlayLabel = new JLabel("Game Play", SwingConstants.CENTER);
-        gamePlayLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        gamePlayLabel.setFont(new Font("Roboto", Font.BOLD, 20));
         JLabel firstLabel = new JLabel("First Turn", SwingConstants.CENTER);
-        firstLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        firstLabel.setFont(new Font("Roboto", Font.BOLD, 16));
         JTextArea firstText = setTextArea("On your first turn, roll the die and select which color /category you want" +
                 " to land on and which of the four spokes you'll move down. If you roll a 4, you will immediately land " +
                 "on a category headquarters and try for a scoring wedge of that color. Whichever color you land on, the " +
@@ -67,7 +67,7 @@ public class InstructionsPage extends JFrame {
                 "in the same move. \n" + "You must move the number of spaces shown on the die.  \n" +
                 "If you answer incorrectly, play passes to the player on your left.");
         JLabel subLabel = new JLabel("Subsequent Turns", SwingConstants.CENTER);
-        subLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        subLabel.setFont(new Font("Roboto", Font.BOLD, 16));
         JTextArea subText = setTextArea("Whenever you answer a category headquarters question correctly, the game " +
                 "will put that colored wedge into your token. But if you answer incorrectly, on your next turn you must " +
                 "move out of that category headquarters for a question before re-entering and trying again for that " +
@@ -76,14 +76,14 @@ public class InstructionsPage extends JFrame {
                 "continue your turn by rolling the die and moving again. \n" + "NOTE: Any number of tokens may occupy " +
                 "the same space at the same time. ");
         JLabel hubLabel = new JLabel("Moving Through the Hub", SwingConstants.CENTER);
-        hubLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        hubLabel.setFont(new Font("Roboto", Font.BOLD, 16));
         JTextArea hubText = setTextArea("You may cut across the board by moving your token up the spokes to the " +
                 "center hub and out again, moving either straight across the hub, or \"turning\" and going down another " +
                 "spoke. If you land by exact count right in the hub - but can't try to win the game because you " +
                 "do not yet have color wedges in all four colors--you may pick whichever category you want for your " +
                 "question. ");
         JLabel winLabel = new JLabel("Winning the Game", SwingConstants.CENTER);
-        winLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        winLabel.setFont(new Font("Roboto", Font.BOLD, 16));
         JTextArea winText = setTextArea("Once you've collected one scoring wedge in each color, make your way to the " +
                 "hub and try to answer the game-winning question. You must land in the hub by exact count; if you " +
                 "overshoot the hub, pick the spoke you want to move down and answer the question in the category you " +
